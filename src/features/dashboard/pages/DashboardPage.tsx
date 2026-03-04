7640c48a76490b8d26e1bdc9efd28bd24564f682
christian-harsana <christian.harsana@gmail.com>
2026-03-05 12:34:32 +1300

    Change the heading of dashboard page into greeting

--- src/features/dashboard/pages/DashboardPage.tsx
+++ src/features/dashboard/pages/DashboardPage.tsx
@@ -1,10 +1,16 @@
+import { useContext } from 'react';
 import Layout from '../../../layouts/Layout';
 import Dashboard from '../components/Dashboard';
+import { AuthContext } from '../../../contexts/AuthContext';
 
 export default function DashboardPage() {
 
+    const {user} = useContext(AuthContext);
+    const heading = user?.name ? `Welcome, ${user.name}` : 'Welcome';
+    
+
     return(
-        <Layout heading={'Dashboard'}>
+        <Layout heading={heading}>
             <Dashboard />
         </Layout>       
     )
