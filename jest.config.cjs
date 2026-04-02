d014a973f5bb1a7e5767cb10e72a3648cc78a3fa
christian-harsana <christian.harsana@gmail.com>
2026-04-02 16:44:00 +1300

    Setup configs

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,16 +1,18 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  testEnvironmentOptions: {
-    customExportConditions: [''],
-  },
-  transform: {
-    '^.+\\.tsx?$': ['ts-jest', {
-      tsconfig: {
-        jsx: 'react-jsx'
-      }
-    }],
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+	preset: 'ts-jest',
+	testEnvironment: 'jest-fixed-jsdom',
+	testEnvironmentOptions: {
+		customExportConditions: [''],
+	},
+	transform: {
+		'^.+\\.tsx?$': [
+			'ts-jest',
+			{
+				tsconfig: {
+					jsx: 'react-jsx',
+				},
+			},
+		],
+	},
+	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
 };
