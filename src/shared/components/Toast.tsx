ce4ce648c1dccc9ca90af6f93b4a148329f95853
christian-harsana <christian.harsana@gmail.com>
2026-04-04 23:00:43 +1300

    Fix react comment inside text nodes error

--- src/shared/components/Toast.tsx
+++ src/shared/components/Toast.tsx
@@ -36,8 +36,8 @@ export default function Toast({ id, text, type, removeToast }: ToastProps) {
 					className="fill-white"
 					aria-hidden="true"
 				>
-					/* !Font Awesome Free v7.1.0 by @fontawesome - https://fontawesome.com License -
-					https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc. */
+					{/* !Font Awesome Free v7.1.0 by @fontawesome - https://fontawesome.com License -
+					https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc. */}
 					<path d="M256 512a256 256 0 1 1 0-512 256 256 0 1 1 0 512zm0-192a32 32 0 1 0 0 64 32 32 0 1 0 0-64zm0-192c-18.2 0-32.7 15.5-31.4 33.7l7.4 104c.9 12.6 11.4 22.3 23.9 22.3 12.6 0 23-9.7 23.9-22.3l7.4-104c1.3-18.2-13.1-33.7-31.4-33.7z" />
 				</svg>
 			);
@@ -53,8 +53,8 @@ export default function Toast({ id, text, type, removeToast }: ToastProps) {
 					className="fill-white"
 					aria-hidden="true"
 				>
-					/* !Font Awesome Free v7.1.0 by @fontawesome - https://fontawesome.com License -
-					https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc. */
+					{/* !Font Awesome Free v7.1.0 by @fontawesome - https://fontawesome.com License -
+					https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc. */}
 					<path d="M256 512a256 256 0 1 1 0-512 256 256 0 1 1 0 512zM374 145.7c-10.7-7.8-25.7-5.4-33.5 5.3L221.1 315.2 169 263.1c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l72 72c5 5 11.8 7.5 18.8 7s13.4-4.1 17.5-9.8L379.3 179.2c7.8-10.7 5.4-25.7-5.3-33.5z" />
 				</svg>
 			);
